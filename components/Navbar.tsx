33ab6a2ccf6e63ff9970a7514ed732231aecf260
cgiovanni96 <cgiovanni1996@gmail.com>
2021-09-27 15:38:10 +0200

    :bug: old map, better experience

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { up } from 'styled-breakpoints'
 import styled from 'styled-components'
 
 const Navbar = () => {
 	return (
 		<Base>
 			<Background>
-				<TigullioBox src={'../assets/map2.svg'} />
+				{/* <TigullioBox src={'../assets/map2.svg'} /> */}
+				<TigullioBox src={'../assets/tigullio.svg'} />
 				{/* <Tigullio /> */}
 			</Background>
 			<Nav>Navbar</Nav>
@@ -23,19 +25,30 @@ const Base = styled.div`
 
 const Background = styled.div`
 	position: absolute;
-	left: -80px;
+	left: -380px;
+	top: -10vh;
 	right: 0;
-	top: -200px;
 
-	margin-top: ${({ theme }) => theme.spacing(-5)};
 	overflow-x: hidden;
 	overflow-y: hidden;
+
+	${up('md')} {
+		top: -60px;
+		left: -40px;
+	}
+
+	${up('lg')} {
+		top: -120px;
+	}
 `
 
 const TigullioBox = styled.img`
-	width: 110%;
-	height: 900px;
-	transform: rotate(-10deg);
+	width: 120%;
+
+	${up('md')} {
+		width: 110%;
+		transform: rotate(-5deg);
+	}
 `
 
 const Nav = styled.nav`
