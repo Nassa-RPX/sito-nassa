48bd04a71b0108c8b31a3e20c6b048aae4bdaefe
Giovanni Costa <cgiovanni1996@gmail.com>
2021-10-25 11:56:35 +0200

    :sparkles: milestone ball

--- components/milestones/MilestoneSection/Milestone.tsx
+++ components/milestones/MilestoneSection/Milestone.tsx
@@ -18,20 +18,50 @@ export const Milestone = ({
 		currentPosition % 2 === 0 ? 'right' : 'left'
 
 	return (
-		<MilestoneSmall position={rightOrLeft(positionInList)} key={milestone.date}>
-			{milestone.title}
-		</MilestoneSmall>
+		<Base position={rightOrLeft(positionInList)}>
+			<MilestoneBall />
+
+			<MilestoneSmall
+				position={rightOrLeft(positionInList)}
+				key={milestone.date}
+			>
+				{milestone.title}
+			</MilestoneSmall>
+		</Base>
 	)
 }
 
+const Base = styled.div<{ position: Position }>`
+	position: relative;
+	margin-bottom: ${({ theme }) => theme.spacing(2)};
+	width: 100%auto;
+`
+
+const MilestoneBall = styled.span`
+	width: 30px;
+	height: 30px;
+	background-color: ${({ theme }) => theme.palette.whiteNassa};
+	border-radius: 100%;
+	border: 4px solid ${({ theme }) => theme.palette.blueNassa};
+
+	z-index: 3;
+
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+
+	position: absolute;
+`
+
 const MilestoneSmall = styled.div<{ position: Position }>`
 	/* width: 300px; */
-	margin-bottom: ${({ theme }) => theme.spacing(2)};
-	margin-left: ${(props) => (props.position === 'left' ? '-300px' : '300px')};
+	/* margin-bottom: ${({ theme }) => theme.spacing(2)}; */
 	padding: ${(props) =>
 		props.position === 'left'
 			? props.theme.spacing(1, 1, 1, 3)
 			: props.theme.spacing(1, 3, 1, 1)};
+	margin-left: ${(props) => (props.position === 'right' ? '300px' : 'inherit')};
+	margin-right: ${(props) => (props.position === 'left' ? '300px' : 'inherit')};
 	background: transparent;
 	border: 4px solid ${({ theme }) => theme.palette.blueNassa};
 	border-radius: 4px;
